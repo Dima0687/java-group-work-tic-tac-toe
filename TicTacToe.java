351e87672566d9c5f023bf7cb13d283ba4bd7955
Dima0687 <eis0687.dietmar@gmail.com>
2023-10-11 14:01:27 +0200

    Dima: created the display Board method, created the 2d array as board and the currentPlayer variable

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,28 @@
 import java.util.Scanner;
 
 public class TicTacToe {
+  private static char[][] board = {
+      { '-', '-', '-' },
+      { '-', '-', '-' },
+      { '-', '-', '-' }
+    };
+    private static char currentPlayer = 'X';
+  
   public static void main(String[] args) {
     
     while(true) {
       
     }
   }
+  
+  private static void displayBoard() {
+    System.out.println("  0 1 2");
+    for (int i = 0; i < 3; i++) {
+        System.out.print(i + " ");
+        for (int j = 0; j < 3; j++) {
+            System.out.print(board[i][j] + " ");
+        }
+        System.out.println();
+    }
+}
 }
