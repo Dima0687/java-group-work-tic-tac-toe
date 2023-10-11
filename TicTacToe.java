8f0e7384a6b59961272ad77208b07817323a831c
Timurs Jalcins <137774733+Timurs99@users.noreply.github.com>
2023-10-11 15:03:29 +0200

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,4 +7,29 @@ public class TicTacToe {
       
     }
   }
+  
+private static boolean checkWinner() {
+
+		for (int i = 0; i < 3; i++) {
+
+			// Row
+			if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+				return true;
+			}
+			// Column
+			if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+				return true;
+			}
+
+			
+		}
+		
+	    if ((board[0][0] == currentPlayer && board [1][1] == currentPlayer && board [2][2] == currentPlayer) ||
+	               (currentPlayer == board [2][0] && currentPlayer == board [1][1] && currentPlayer == board [0][2])) {
+	               return true;
+	           }
+	    return false;
+	    
+	}
+
 }
