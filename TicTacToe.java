cf12e61754b75b56e029aa215d08c4cfd4094f92
Dietmar E <86421371+Dima0687@users.noreply.github.com>
2023-10-11 15:08:05 +0200

    Dima created switch player method (#2)
    * Dima: created the display Board method, created the 2d array as board and the currentPlayer variable
    
    * Dima: created the display Board method, created the 2d array as board and the currentPlayer variable
    
    * Dima: added our brainstorming png
    
    * Dima: created the switchPlayer method and added some additional ideas as comments

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,49 @@
 import java.util.Scanner;
 
 public class TicTacToe {
+  private static char[][] board = { { '-', '-', '-' }, { '-', '-', '-' }, { '-', '-', '-' } };
+  private static char currentPlayer = 'X';
+
   public static void main(String[] args) {
+
+    displayBoard();
     
-    while(true) {
+    while (true) {
+      displayBoard(); // refresh the board with new data from the board
+      
+      /*
+      if(checkWinner() || isNoMoreMovesPossible()) { // check if we have a winner or if we have the board full
+        break;
+      }
+      
+      */
+      
+      switchPlayer(); // round is over, switch to the next player
       
     }
+    
+    /*
+     if(checkWinner()) {
+        System.out.println("Player " + currentPlayer + " wins!");
+     } else {
+        System.out.println("It's a draw!");
+     }
+     */
+  }
+
+  private static void displayBoard() {
+    System.out.println("  0 1 2");
+    for (int i = 0; i < 3; i++) {
+      System.out.print(i + " ");
+      for (int j = 0; j < 3; j++) {
+        System.out.print(board[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+  
+  private static void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
   }
   
 private static boolean checkWinner() {
